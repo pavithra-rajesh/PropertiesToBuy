00c936078f2ed0f912f12ee0bbc2b94cb7fca504
pavithra srinivasan <psrinivasan@independer.nl>
2020-11-21 10:49:38 +0100

    Removed startup weather forecast related components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { WeatherComponent } from './weather/weather.component';
 import { MakelaarsComponent } from './makelaars/makelaars.component';
 
 @NgModule({
@@ -18,9 +16,7 @@ import { MakelaarsComponent } from './makelaars/makelaars.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
     MakelaarsComponent,
-    WeatherComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -30,9 +26,7 @@ import { MakelaarsComponent } from './makelaars/makelaars.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'makelaars', component: MakelaarsComponent},
-      { path: 'weather', component: WeatherComponent},
       { path: '**', redirectTo: 'home' }
     ])
   ],
